ed79e6a370d63d4cdb401e967e09edc38c998748
mrj <mrj19981998@gmail.com>
2020-09-08 18:33:10 +0530

    Updates

---
+++ precache-manifest.3db8c2c99b7f91570702c6cfb8cefa71.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b79911147572ae6454ecba9aea8297",
+    "url": "/mrjblog/index.html"
+  },
+  {
+    "revision": "d5ba30e73563b5e6b33f",
+    "url": "/mrjblog/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "581c0108ed79223094b4",
+    "url": "/mrjblog/static/css/main.4d2e8f2a.chunk.css"
+  },
+  {
+    "revision": "d5ba30e73563b5e6b33f",
+    "url": "/mrjblog/static/js/2.f0ead832.chunk.js"
+  },
+  {
+    "revision": "548f956030c021697af0b3ec6efc9938",
+    "url": "/mrjblog/static/js/2.f0ead832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581c0108ed79223094b4",
+    "url": "/mrjblog/static/js/main.c1696819.chunk.js"
+  },
+  {
+    "revision": "a4a3056d65ac43b5e19f",
+    "url": "/mrjblog/static/js/runtime-main.0059c829.js"
+  },
+  {
+    "revision": "5bca6f0c7c32e7200c7d030a48ce271b",
+    "url": "/mrjblog/static/media/aa.5bca6f0c.png"
+  },
+  {
+    "revision": "c7cf4ba64076454000ed8df1743c2cd5",
+    "url": "/mrjblog/static/media/c.c7cf4ba6.png"
+  }
+]);
